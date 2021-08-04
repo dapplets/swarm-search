782a6935d6764c0860b53964bb2db3a709319cae
Alexander Sakhaev <alsakhaev@gmail.com>
2021-08-04 17:19:01 +0300

    feat: added 300ms delay for mocked API

--- packages/search-dapplet/src/api.ts
+++ packages/search-dapplet/src/api.ts
@@ -1,5 +1,8 @@
+
 export class Api {
+
     async search() {
+        await new Promise(r => setTimeout(r, 300));
         return [{
             title: "Search result from Ethereum Swarm",
             views: "10M",
@@ -7,7 +10,17 @@ export class Api {
             channel: "Ethereum Swarm",
             description: "We are injecting search results into YouTube and Google.",
             badges: [{
-              label: 'AVAILABLE IN SWARM',
+              label: 'SWARM',
+              color: '#ffc300'
+            }]
+        }, {
+            title: "Hidden video from Darknet",
+            views: "1M",
+            date: "1 month ago",
+            channel: "Ethereum Swarm",
+            description: "We are injecting search results into YouTube and Google.",
+            badges: [{
+              label: 'SWARM',
               color: '#ffc300'
             }]
         }];
--- packages/search-dapplet/src/index.ts
+++ packages/search-dapplet/src/index.ts
@@ -2,34 +2,6 @@ import { } from '@dapplets/dapplet-extension';
 import { Api } from './api';
 import THUMBNAIL_IMG from './icons/thumbnail.png';
 
-const searchResults = [
-  {
-    title: 'Typesss of Clouds | NOAA SciJinks - All About Weather',
-    link: 'https://scijinks.gov/clouds/',
-    description:
-      'Mammatus clouds. Mammatus clouds are actually altocumulus, cirrus,\
-      cumulonimbus, or other types of clouds that have these pouch-like shapes hanging \
-      out of the bottom. The pouches are created when cold air within the cloud sinks down \
-      toward the Earth. Weather prediction: Severe weather might be on its way!',
-  },
-  {
-    title: 'Clouds—facts and information - Science',
-    link: 'https://www.nationalgeographic.com/science/article/clouds-1',
-    description:
-      'Altostratus clouds may portend a storm. Nimbostratus clouds are thick \
-      and dark and can produce both rain and snow. Low clouds fall into four divisions: \
-      cumulus, stratus, cumulonimbus, and ...',
-  },
-  {
-    title: 'Types of Clouds | Live Science',
-    link: 'https://www.livescience.com/29436-clouds.html',
-    description:
-      'Clouds of great vertical development: These are the cumulonimbus clouds, \
-      often called a thunderhead because torrential rain, vivid lightning and thunder come \
-      from it. The tops of such clouds may ...',
-  },
-];
-
 @Injectable
 export default class GoogleFeature {
 
@@ -41,11 +13,12 @@ export default class GoogleFeature {
   activate() {
     const { button, result } = this.adapter.exports;
     this.adapter.attachConfig({
-      SEARCH_RESULT_GROUP: async () => {
-        const search = await this._api.search();
-        return search.map(x => result({
+      SEARCH_RESULT_GROUP: async (ctx) => {
+        const results = await this._api.search();
+        return results.map(x => result({
           "DEFAULT": {
             ...x,
+            title: ctx.query,
             img: THUMBNAIL_IMG,
             exec: (ctx, me) => {
               window.open('https://gateway.ethswarm.org/files/6995fd78ab680c53d6cc4003082e5cf9b5225644ae6e0f1892ecf966075f0248', '_blank')
