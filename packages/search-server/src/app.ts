4b4e8d02f26d758cdc7eea3e3fe455ca5b0b8688
Alexander Sakhaev <alsakhaev@gmail.com>
2021-08-11 21:11:15 +0300

    fix: api doesn't work in production

--- packages/search-server/src/app.ts
+++ packages/search-server/src/app.ts
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/', routes);
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, '../src/public')));
 
 app.use((req, res) => {
     res.status(404).json({
--- packages/search-server/src/controllers/rss.ts
+++ packages/search-server/src/controllers/rss.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 import xml from 'xml';
 
 export function get(req: Request, res: Response) {
-    const data = fs.readFileSync(path.join(__dirname, '../search-results.json'), { encoding: 'utf-8' });
+    const data = fs.readFileSync(path.join(__dirname, '../../src/search-results.json'), { encoding: 'utf-8' });
     const xmlString = xml(JSON.parse(data), { declaration: true });
     res.setHeader('Content-Type', 'text/xml');
     res.send(xmlString);
